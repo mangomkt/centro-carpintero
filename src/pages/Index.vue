ef336c6f9badcd0171386d87f25cf71a66217ac5
curtisrg <curtisg@roadsidemultimedia.com>
2020-10-24 15:57:19 -0500

    updating HP

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,75 +1,31 @@
 <template>
   <Layout>
-    <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16">
-      <div class="text-4xl font-bold w-full sm:w-3/5 text-center sm:text-left">
-        <div class="leading-tight">Gridsome Portfolio Starter</div>
-        <div class="text-green-700 leading-tight">Powered by Vue.js &amp; Tailwind v1</div>
+    <div class="hero">
+      <div class="container-inner mx-auto flex flex-col sm:flex-row justify-between py-40">
+        <div class="font-bold w-full text-center sm:text-center">
+          <div class="text-6xl leading-tight">Centro Carpintero</div>
+          <div class="text-2xl text-green-700 leading-tight">Home renovations and fine carpentry</div>
+        </div>
+      </div> <!-- end hero -->
+    </div>
+    <div class="container-inner py-20 mx-auto">
+      <div class="font-bold w-full text-center sm:text-center mb-10">
+        <div class="text-4xl leading-tight">Why Choose Centro Carpintero?</div>
       </div>
-      <div class="mt-8 sm:mt-0">
-        <g-image src="../../static/developer.svg" alt="hero" class="mx-auto sm:mx-0" />
-      </div>
-    </div> <!-- end hero -->
-
-    <div class="container-inner mx-auto">
-      <p class="text-lg sm:text-xl">This gridsome theme was forked from my <a href="https://andremadarang.com">personal website</a>. I added more features and decided to release it as an open source project. Check it out on <a href="https://github.com/drehimself/gridsome-portfolio-starter">GitHub</a> or check out the <a href="https://www.youtube.com/watch?v=uHo6o1TNQeE">screencast I did</a>. Feel free to use it for your own site or projects! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, repellat. Dolores iure, quas perspiciatis rerum quaerat atque eum repellat autem.</p>
+      <p class="text-lg sm:text-xl">Centro Carpintero is an artistic carpentry business focused on functional furnishing and elegant design in high-quality wood. If you are looking to think outside the box to better use your residence’s space, then you will love our integrative wood-crafted solutions!</p>
+      <p class="text-lg sm:text-xl">Infuse your home or business with our extensive experience in classy wood designs that stand the test of time and add value and personality to your estate. With a passion for carpentry and commitment to the quality of our work, we will partner with you to propose combinations of the latest trends in functionality, style, and beauty for all types of fine furniture and woodworking. Here is a glimpse of what we do:</p>
 
       <div class="flex justify-between items-center py-6">
-        <div class="w-full sm:w-1/2 px-8 py-8 sm:py-0">
+        <div class="w-full sm:w-1/3 px-8 py-8 sm:py-0">
           <g-image src="../../static/mind_map.svg" alt="browser" />
         </div>
-        <div class="w-1/2 px-8 hidden sm:flex justify-end">
+        <div class="w-1/3 px-8 hidden sm:flex justify-end">
+          <g-image src="../../static/code_review.svg" alt="mobile" />
+        </div>
+        <div class="w-1/3 px-8 hidden sm:flex justify-end">
           <g-image src="../../static/code_review.svg" alt="mobile" />
         </div>
       </div>
-    </div>
-
-    <div class="overflow-x-hidden">
-      <div class="projects container-inner mx-auto text-xl border-t border-gray-500 border-b py-16 mb-16 relative">
-        <h2 class="font-bold mb-6" id="projects">Here are some projects I've worked on:</h2>
-
-        <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
-          <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
-        </div>
-
-        <ul class="text-lg sm:text-xl space-y-6">
-          <li class="checkmark">
-            <div>Project One</div>
-            <div class="text-lg text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ab nam animi iure nemo exercitationem a, eius non culpa itaque!</div>
-          </li>
-          <li class="checkmark">
-            <div>Project Two</div>
-            <div class="text-lg text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ab nam animi iure nemo exercitationem a, eius non culpa itaque!</div>
-          </li>
-          <li class="checkmark">
-            <div>Project Three</div>
-            <div class="text-lg text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ab nam animi iure nemo exercitationem a, eius non culpa itaque!</div>
-          </li>
-          <li class="checkmark">
-            <div>Project Four</div>
-            <div class="text-lg text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ab nam animi iure nemo exercitationem a, eius non culpa itaque!</div>
-          </li>
-
-        </ul>
-      </div> <!-- end projects -->
-    </div>
-
-    <div class="overflow-x-hidden border-gray-200 border-b">
-      <div class="get-to-know-me container-inner mx-auto text-xl pb-16 relative">
-        <h2 class="font-bold mb-6" id="about">Get to know me:</h2>
-
-        <div class="absolute left-0" style="top: 50px; transform: translateX(-100%)">
-          <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
-        </div>
-
-        <div class="flex flex-col sm:flex-row justify-between items-center mb-16">
-          <div><g-image src="../../static/avatar.jpg" alt="avatar" class="w-32 h-32 rounded-full mb-8 lg:mb-0" /></div>
-          <div class="flex-1 text-lg sm:text-xl ml-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum quae architecto minus molestias iusto ducimus fuga quo quia libero dignissimos, fugiat modi assumenda debitis laboriosam.</div>
-        </div>
-
-        <div class="responsive-container">
-          <iframe class="responsive-iframe w-full h-full top-0 left-0 border-0" src="https://www.youtube.com/embed/uHo6o1TNQeE" style="border:0;" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-      </div> <!-- end get-to-know me -->
     </div>
 
     <div class="overflow-x-hidden">
@@ -119,7 +75,26 @@
         </div>
       </div> <!-- end contact-me -->
     </div>
+    <div class="container-inner mx-auto py-20">
+      <div class="flex">
+        <div v-for="post in $page.posts.edges" :key="post.id" class="w-1/4 px-8 post border-gray-400 border-b mb-12">
+          <h2 class="text-3xl font-bold"><g-link :to="post.node.path" class="text-copy-primary">{{ post.node.title }}</g-link></h2>
+          <div class="text-copy-secondary mb-4">
+            <span>{{ post.node.date }}</span>
+            <span> &middot; </span>
+            <span>{{ post.node.timeToRead }} min read</span>
+          </div>
 
+          <div class="text-lg mb-4">
+            {{ post.node.summary }}
+          </div>
+
+          <div class="mb-8">
+            <g-link :to="post.node.path" class="font-bold uppercase">Read More</g-link>
+          </div>
+        </div> <!-- end post -->
+      </div>
+    </div>
     <div class="newsletter bg-background-tertiary">
       <div class="container-inner mx-auto py-16 pb-8 text-center textl-xl">
         <h2 class="text-2xl font-bold mb-8">Join my Newsletter</h2>
@@ -145,7 +120,27 @@
 
   </Layout>
 </template>
-
+<page-query>
+query Posts ($page: Int) {
+  posts: allPost (sortBy: "date", order: DESC, perPage: 4, page: $page) @paginate {
+    totalCount
+    pageInfo {
+      totalPages
+      currentPage
+    }
+    edges {
+      node {
+        id
+        title
+        date (format: "MMMM D, Y")
+        summary
+        timeToRead
+        path
+      }
+    }
+  }
+}
+</page-query>
 <script>
 export default {
   metaInfo: {
