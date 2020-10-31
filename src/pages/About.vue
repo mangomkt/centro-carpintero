a17dd7ff3271c6919eef2a5897e67872e4012492
curtisrg <curtisg@roadsidemultimedia.com>
2020-10-30 23:09:55 -0600

    home and about updates

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -19,77 +19,77 @@
         </div>
       </div>
       <div class="flex flex-wrap justify-between items-center py-6">
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team1.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">Santiago Ramirez Rey</div>
           <div class="leading-tight mb-4">Expert Carpenter</div>
         </div>
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team2.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">Victorino Rodriguez Cruz</div>
           <div class="leading-tight mb-4">Expert Carpenter</div>
         </div>
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team4.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">David Hernandez Gil</div>
           <div class="leading-tight mb-4">Expert Carpenter</div>
         </div>
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team7.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">Jose Enrique Chavez Orozco</div>
           <div class="leading-tight mb-4">Expert Carpenter</div>
         </div>
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team13.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">Ivan Alejandro Aguilar Gomez</div>
           <div class="leading-tight mb-4">Expert Carpenter</div>
         </div>
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team11.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">Lucio Perez De Nova</div>
           <div class="leading-tight mb-4">Expert Carpenter</div>
         </div>
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team12.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">Sabdiel Luviano Alarcon</div>
           <div class="leading-tight mb-4">Expert Carpenter</div>
         </div>
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team5.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">Kevin Heriberto Moran Perez</div>
           <div class="leading-tight mb-4">Carpenter Assistant</div>
         </div>
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team8.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">Gabriel Abath Moran Perez</div>
           <div class="leading-tight mb-4">Assistant</div>
         </div>
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team14.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">Isidro Curiel Garcias</div>
           <div class="leading-tight mb-4">Lacquering Specialist</div>
         </div>
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team3.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">Jaime Espindola</div>
           <div class="leading-tight mb-4">Lacquering Specialist</div>
         </div>
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team6.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">Juan David Garcia Lopez</div>
           <div class="leading-tight mb-4">Supervisor</div>
         </div>
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team9.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">Susana Mariela De Leon Ramirez</div>
           <div class="leading-tight">Supervisor</div>
         </div>
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team10.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">Karina Lisset Jacome Villanueva</div>
           <div class="leading-tight">Administration</div>
         </div>
-        <div class="w-full sm:w-1/4 md:1/2 px-8 py-8 sm:py-0 text-center">
+        <div class="w-full sm:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/team15.jpg" alt="browser" />
           <div class="text-green font-bold leading-tight mt-4">Lizeth Estefani Aguilar Servin</div>
           <div class="leading-tight">General Manager</div>
