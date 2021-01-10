461d7279d97c032890e94eb353b2f10c8508b252
mangomkt <cg@mangomkt.com>
2021-01-09 18:15:23 -0600

    updating ES Tags

--- src/templates/EsTag.vue
+++ src/templates/EsTag.vue
@@ -1,9 +1,9 @@
 <template>
   <Esp>
     <div class="container-inner mx-auto my-16">
-      <h2 class="text-4xl font-bold mb-8 border-b">Tag: {{ $page.tag.title }}</h2>
+      <h2 class="text-4xl font-bold mb-8 border-b">etiqueta: {{ $page.etiqueta.title }}</h2>
 
-      <div v-for="post in $page.tag.belongsTo.edges" :key="post.node.id" class="post border-gray-400 border-b mb-12">
+      <div v-for="post in $page.etiqueta.belongsTo.edges" :key="post.node.id" class="post border-gray-400 border-b mb-12">
         <h2 class="text-3xl font-bold"><g-link :to="post.node.path" class="text-copy-primary">{{ post.node.title }}</g-link></h2>
         <div class="text-copy-secondary mb-4">
           <span>{{ post.node.date }}</span>
@@ -21,10 +21,10 @@
       </div>
 
       <pagination-posts
-        v-if="$page.tag.belongsTo.pageInfo.totalPages > 1"
-        :base="`/tag/${$page.tag.title}`"
-        :totalPages="$page.tag.belongsTo.pageInfo.totalPages"
-        :currentPage="$page.tag.belongsTo.pageInfo.currentPage"
+        v-if="$page.etiqueta.belongsTo.pageInfo.totalPages > 1"
+        :base="`/etiqueta/${$page.etiqueta.title}`"
+        :totalPages="$page.etiqueta.belongsTo.pageInfo.totalPages"
+        :currentPage="$page.etiqueta.belongsTo.pageInfo.currentPage"
       />
 
     </div>
@@ -33,7 +33,7 @@
 
 <page-query>
 query etiqueta ($id: ID!, $page: Int) {
-  tag: etiqueta (id: $id) {
+  etiqueta: etiqueta (id: $id) {
     title
     belongsTo (page: $page, perPage: 3) @paginate {
       totalCount
