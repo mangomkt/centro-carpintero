df4194e0c472b12f19872e82106118d0d0127efc
DESKTOP-8B049TT\Jenny <cg@mangomkt.com>
2021-03-03 15:40:18 -0600

    updated gallery

--- src/pages/Gallery.vue
+++ src/pages/Gallery.vue
@@ -8,38 +8,100 @@
         </div>
       </div> <!-- end hero -->
     </div>
-    <div class="container-inner mx-auto py-20">
+    <div class="text-4xl py-8 text-black font-light leading-tight" style="text-align:center">Kitchens</div>
+    <div class="container-inner mx-auto py-50">
+
       <div class="flex flex-wrap justify-between items-center py-6">
+
         <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/gallery/RENOVATED-KITCHEN.jpg" alt="Custom kitchen remodeling" />
           <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Kitchen Remodeling</div>
-        </div>
-        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/CUSTOM-WOOD-CABINET.jpg" alt="Fine Carpentry Cabinet" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Fine Carpentry Cabinet</div>
-        </div>
-        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/CUSTOM-WOOD-BED-FRAME.jpg" alt="Custom Woodworking Bed frame" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Woodworking Bed frame</div>
         </div>
         <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/gallery/KITCHEN-CABINETS.jpg" alt="Beautiful Kitchen Woodworking" />
           <div class="text-green font-bold leading-tight mt-4 mb-8">Beautiful Kitchen Woodworking</div>
         </div>
         <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/MAKEUP.jpg" alt="Beautiful Wooden Makeup Desk / Dresser" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Beautiful Wooden Makeup Desk / Dresser</div>
+          <g-image src="../../static/gallery/RENOVATED-KITCHEN-A1.jpg" alt="Modern Kitchen Look" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Modern Kitchen Look</div>
         </div>
         <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/CUSTOM-WOOD-BEDFRAME2.jpg" alt="Parota Wood Headboard" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Parota Wood Headboard</div>
+          <g-image src="../../static/gallery/RENOVATED-UPDATING-KITCHEN.jpg" alt="Installing New Kitchen Island" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Installing New Kitchen Island</div>
+        </div>
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/KITCHEN-CABINETSA2.jpg" alt="Modern Kitchen Pantry" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Modern Kitchen Pantry</div>
+        </div>
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/RENOVATED-NEW-CABINETS.jpg" alt="Custom Kitchen Cabinets" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Kitchen Cabinets</div>
+        </div>
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/New-island2.jpg" alt="Custom Kitchen Island" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Kitchen Island</div>
+        </div>
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/UPDATED-KITCHEN-SINK.jpg" alt="Luxury Kitchen Look" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Luxury Kitchen Look</div>
+        </div>
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/New-island.jpg" alt="Custom Kitchen Island Drawers" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Kitchen Island Drawers</div>
+        </div> 
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/New-k-pantry.jpg" alt="Custom Pantries" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Pantries</div>
+        </div>
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/modern-kitchen.jpg" alt="Modern Kitchen Look" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Modern Kitchen Look</div>
+          </div>
+          <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/CUSTOM-WOOD-CABINET.jpg" alt="Fine Carpentry Cabinet" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Fine Carpentry Cabinet</div>
+        </div>
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/RENOVATED-NEW-BAR.jpg" alt="Custom Livingroom Bar" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Livingroom Bar</div>
+        </div>
+      </div>
+      <h2 class="text-4xl text-Black font-light leading-tight" style="text-align:center">Bedrooms and Bathrooms</h2>
+      <div class="flex flex-wrap justify-between items-center py-6">
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/CUSTOM-WOOD-BED-FRAME.jpg" alt="Custom Woodworking Bed frame" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Woodworking Bed frame</div>
+        </div>
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/UPDATED-BEDROOM-CLOSETS.jpg" alt="Updated Bedroom closets" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Updated Bedroom closets</div>
+        </div>
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/SHOWER.jpg" alt="Renovated Bathroom Shower" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Renovated Bathroom Shower</div>
+        </div>
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/bed-frame.jpg" alt="Wood Bed Frame" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Wood Bed Frame</div>
+        </div>
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/RENOVATED-BATHROOM-TILES.jpg" alt="Bathroom Wall Tiles" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Bathroom Wall Tiles</div>
         </div>
         
         <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/BENCH.jpg" alt="Fine Woodworking Bench" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Fine Woodworking Bench</div>
+          <g-image src="../../static/gallery/RENOVATED-SINK2.jpg" alt="Updated Bathroom Sink" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Updated Bathroom Sink</div>
+        </div>
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/custom-sinks.jpg" alt="Custom Sinks"/>
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Sinks</div>
         </div>
 
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/RENOVATED-SINK1.jpg" alt="Updated Bathroom Sink" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Updated Bathroom Sink</div>
+        </div>
         <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/gallery/RENOVATED-BATHROOM-TILES2.jpg" alt="Bathroom Floor Tiles" />
           <div class="text-green font-bold leading-tight mt-4 mb-8">Bathroom Floor Tiles</div>
@@ -48,6 +110,22 @@
         <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/gallery/custom-bathroom-designs.jpg" alt="Trendy bathroom desings" />
           <div class="text-green font-bold leading-tight mt-4 mb-8">Trendy bathroom desings</div>
+        </div>
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/CUSTOM-WOOD-BEDFRAME2.jpg" alt="Parota Wood Headboard" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Parota Wood Headboard</div>
+        </div>
+      </div>
+      <h2 class="text-4xl text-Black font-light leading-tight" style="text-align:center">Custom Wood</h2>
+      <div class="flex flex-wrap justify-between items-center py-6">
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/MAKEUP.jpg" alt="Beautiful Wooden Makeup Desk / Dresser" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Beautiful Wooden Makeup Desk / Dresser</div>
+        </div>
+
+        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
+          <g-image src="../../static/gallery/BENCH.jpg" alt="Fine Woodworking Bench" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Fine Woodworking Bench</div>
         </div>
 
         <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
@@ -61,86 +139,23 @@
         </div>
 
         <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/custom-sinks.jpg" alt="Custom Sinks"/>
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Sinks</div>
+          <g-image src="../../static/gallery/desk.jpg" alt="Custom Desk" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Desk</div>
         </div>
 
         <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/RENOVATED-SINK1.jpg" alt="Updated Bathroom Sink" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Updated Bathroom Sink</div>
-        </div>
-        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/RENOVATED-KITCHEN-A1.jpg" alt="Modern Kitchen Look" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Modern Kitchen Look</div>
+          <g-image src="../../static/gallery/custom-shelfs-and-desk.jpg" alt="Custom Shelfs And Desk" />
+          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Shelfs And Desk</div>
         </div>
 
-      <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/KITCHEN-CABINETSA2.jpg" alt="Modern Kitchen Pantry" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Modern Kitchen Pantry</div>
-        </div>
         <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/gallery/CUSTOM-WOOD-NIGHT-TABLES.jpg" alt="Wood Night Tables" />
           <div class="text-green font-bold leading-tight mt-4 mb-8">Wood Night Tables</div>
         </div>
-        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/UPDATED-KITCHEN-SINK.jpg" alt="Luxury Kitchen Look" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Luxury Kitchen Look</div>
-        </div>
-        
-        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/RENOVATED-BATHROOM-TILES.jpg" alt="Bathroom Wall Tiles" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Bathroom Wall Tiles</div>
-        </div>
-        
-        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/RENOVATED-SINK2.jpg" alt="Updated Bathroom Sink" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Updated Bathroom Sink</div>
-        </div>
 
-        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/New-island.jpg" alt="Custom Kitchen Island Drawers" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Kitchen Island Drawers</div>
-        </div> 
-
-        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/New-island2.jpg" alt="Custom Kitchen Island" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Kitchen Island</div>
-        </div>
-
-        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/New-k-pantry.jpg" alt="Custom Pantries" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Pantries</div>
-        </div>
-
-        
         <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
           <g-image src="../../static/gallery/New-livingroom.jpg" alt="Custom Livingroom Cabinets" />
           <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Livingroom Cabinets</div>
-        </div>
-
-        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/SHOWER.jpg" alt="Renovated Bathroom Shower" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Renovated Bathroom Shower</div>
-        </div>
-
-        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/RENOVATED-NEW-CABINETS.jpg" alt="Custom Kitchen Cabinets" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Kitchen Cabinets</div>
-        </div>
-
-        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/RENOVATED-NEW-BAR.jpg" alt="Custom Livingroom Bar" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Custom Livingroom Bar</div>
-        </div>
-
-        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/RENOVATED-UPDATING-KITCHEN.jpg" alt="Installing New Kitchen Island" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Installing New Kitchen Island</div>
-        </div>
-
-        <div class="w-full lg:w-1/4 md:w-1/2 px-8 py-8 sm:py-0 text-center">
-          <g-image src="../../static/gallery/UPDATED-BEDROOM-CLOSETS.jpg" alt="Updated Bedroom closets" />
-          <div class="text-green font-bold leading-tight mt-4 mb-8">Updated Bedroom closets</div>
         </div>
       </div>
     </div>
